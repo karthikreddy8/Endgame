5026a7f81b8279ed3f88663b99dd01e38f34dc16
karthikreddy82 <karthikreddy.v81@gmail.com>
2020-12-02 14:27:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var readlinesync=require("readline-sync");
+
+const chalk=require("chalk");
+
+var username=readlinesync.question(chalk.bold(chalk.green("what is your name? ")))
+score=0;
+
+console.log(chalk.blue("wellcome "+ username +" Do you know karthik? "))
+//branching
+
+function play(question,answer){
+
+  var useranswer=readlinesync.question(question);
+  
+  if(useranswer===answer){
+
+    console.log(chalk.bold("Right"));
+    score=score+1;
+  }else{
+    
+    console.log("wrong");
+    
+    score
+  }
+  console.log(" your corent score",score)
+  
+  console.log("-----------")
+
+}
+
+//array of objects
+var questions=[{
+  question: "where do i live? ",
+  answer:"Bangalure"
+},{
+  question:"my faorite superhero would be? ",
+  answer:"Iron man"
+
+},{
+  question:"where do i work? ",
+  answer:"hydrilla"
+}];
+
+//loop
+
+for(var i=0; i<questions.length; i++){
+  var currentQuestion = questions[i];
+  play(currentQuestion.question,currentQuestion.answer)
+  
+}
